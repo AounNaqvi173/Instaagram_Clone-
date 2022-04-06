af789bcb6263f0ef1d50d11604cfc36db47286d6
Aoun Naqvi <18321519-173@uog.edu.pk>
2022-04-07 00:20:35 +0500

    Second Commit: Added firebase

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKDT0_-fqHsw7HKNJrrX0jyOfvotBKB7w",
+  authDomain: "instagram-clone-f16e3.firebaseapp.com",
+  projectId: "instagram-clone-f16e3",
+  storageBucket: "instagram-clone-f16e3.appspot.com",
+  messagingSenderId: "845743723168",
+  appId: "1:845743723168:web:a63d5b03f5caa18826afc5",
+  measurementId: "G-BCZ3SJPCVN"
+}; 
+
+if (firebase.apps.lenght ===0)
+{
+  firebase.initializeApp(firebaseConfig)
+}
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import { StyleSheet, Text, View } from 'react-native';
